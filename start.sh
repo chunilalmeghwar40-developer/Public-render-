6dc02f1ec5818a9d1485972cc260e34e1b1d2be6
chunilalmeghwar40-developer <chunilalmeghwar40@gmail.com>
2026-05-13 06:29:48 +0530

    Refactor start.sh for server setup and execution

--- start.sh
+++ start.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-# unzip server
-unzip -o bedrock-server.zip -d server
+# packages folder
+mkdir -p /app/server
 
-cd server
+cd /app/server
+
+# download latest bedrock server
+curl -L https://minecraft.azureedge.net/bin-linux/bedrock-server-1.21.100.7.zip -o bedrock.zip
+
+# unzip
+unzip -o bedrock.zip
 
 chmod +x bedrock_server
 
-# start bedrock server
+# start server
 LD_LIBRARY_PATH=. ./bedrock_server &
 
-cd ..
+cd /app
 
 # download playit
 curl -L https://github.com/playit-cloud/playit-agent/releases/latest/download/playit-linux-amd64 -o playit
